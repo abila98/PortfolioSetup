463682525808b4c743ac4cb8c1a86cc606bd99f3
abila98 <abilanadar1998@gmail.com>
2024-02-07 04:39:28 -0800

    backend

---
+++ terraform/backend.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "tfstate_bucket" {
+  bucket = "kohee-terraform-state" 
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name = "kohee-s3"
+  }
+} 
+
+
+
+
+resource "aws_dynamodb_table" "remotestate_table" {
+  name           = "kohee-terraform-state-lock"
+  hash_key       = "LockID"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "kohee-Dynamo"
+  }
+
+}
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,18 @@
 #  required_version = ">= 1.0"
 #}
 
+
 provider "aws" {
-  # version = "~> 3.0"
   region = "us-west-1"
 }
+
+terraform {
+  backend "s3" {
+    region         = "us-west-1"
+    bucket         = "kohee-terraform-state"
+    key            = "state.tfstate"
+    dynamodb_table = "kohee-terraform-state-lock"
+    encrypt        = true
+  }
+}
+
