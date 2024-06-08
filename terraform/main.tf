f2be0103de5545e92bb9769f01ac28aeac0546d6
Abila <41573378+abila98@users.noreply.github.com>
2024-06-08 22:36:51 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
 module "asg" {
   source             = "./asg"
   vpc_id             = module.vpc.vpc_id
-  ami_id             = var.ami_id
+  ami_id             = data.aws_ami.latest_ami.id
   security_group_id  = module.vpc.security_group_id
   public_subnet_ids  = module.vpc.public_subnet_ids
   private_subnet_ids = module.vpc.private_subnet_ids
