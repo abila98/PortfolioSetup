203b45611144185edd5f2650d318ecb5c630cb26
Abila <41573378+abila98@users.noreply.github.com>
2024-06-08 22:17:20 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -16,7 +16,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     region         = "us-west-1"
-    bucket         = "portfolio-terraform-bucket-v1"
+    bucket         = "portfolio-terraform-s3-bucket-v1"
     key            = "state.tfstate"
     dynamodb_table = "portfolio-terraform-state-lock"
     encrypt        = true
